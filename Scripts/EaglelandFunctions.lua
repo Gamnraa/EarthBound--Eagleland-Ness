9cac9c56fd4a6ca869c9b44922fc7480be69377f
Gamnraa <ltaz1466@gmail.com>
2023-04-09 23:54:58 -0400

    Create EaglelandFunctions.lua

---
+++ Scripts/EaglelandFunctions.lua
@@ -0,0 +1,34 @@
+-- EaglelandFunctions
+-- Author: Anagram
+-- DateCreated: 4/9/2023 10:52:38 PM
+--------------------------------------------------------------
+
+--breaking away from Hungarian notation, you will NOT take me back!
+GLOBAL_EAGLELAND_SUZERAINS = ReadCustomData("GRAM_EAGLELAND_SUZERAINS") or {}
+GLOBAL_FREE_CITY_STATES = ReadCustomData("GRAM_FREE_CITY_STATES") or {}
+
+GLOBAL_EAGLELAND = GameInfo.Civilizations["CIVILIZATION_GRAM_EAGLELAND"]
+
+function InitNewGame()
+	for i = 0, GameDefines.MAX_PLAYERS-1, 1 do
+		local player = Players[i]
+		if player:WasEverAlive() and player:IsAlive() then
+			if player:GetCivilizationType() == GLOBAL_EAGLELAND  then
+				GLOBAL_EAGLELAND_SUZERAINS[i] = {}
+			end
+		
+			if not(player:IsFreeCities() or player:IsMajor() or player:IsBarbarian()) then
+				GLOBAL_FREE_CITY_STATES[i] = Game.GetCurrentGameTurn()
+			end
+		end
+	end
+
+	if #GLOBAL_EAGLELAND_SUZERAINS > 0 then
+		--TODO Hook our functions
+	end
+
+	WriteCustomData("GRAM_EAGLELAND_SUZERAINS", GLOBAL_EAGLELAND_SUZERAINS)
+	WriteCustomData("GRAM_FREE_CITY_STATES", GLOBAL_FREE_CITY_STATES)
+end
+
+if #GLOBAL_EAGLELAND_SUZERAINS == 0 then InitNewGame() end
