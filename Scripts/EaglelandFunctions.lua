47521d96c0641662ea2d3f1c1741e9638d9236eb
Gamnraa <ltaz1466@gmail.com>
2023-04-10 21:03:02 -0400

    Update EaglelandFunctions.lua

--- Scripts/EaglelandFunctions.lua
+++ Scripts/EaglelandFunctions.lua
@@ -9,20 +9,28 @@ GLOBAL_EAGLELAND_SUZERAINS = {}
 GLOBAL_FREE_CITY_STATES = {}
 
 --GLOBAL_EAGLELAND = GameInfo.Civilizations["CIVILIZATION_GRAM_EAGLELAND"]
+function GetTableLength(t)
+	local i = 0
+	for _, _ in pairs(t) do
+		i = i + 1
+	end
+	return i
+end
+
 
 function IsEagleland(id)
-	return PlayersConfigurations[id]:GetCivilizationTypeName() == "CIVILIZATION_GRAM_EAGLELAND"
+	return PlayerConfigurations[id]:GetCivilizationTypeName() == "CIVILIZATION_GRAM_EAGLELAND"
 end
 
 function UpdateSuzerainStatus()
-	for i = 20, GameDefines.MAX_PLAYERS-1, 1 do
+	for _, i in pairs(PlayerManager.GetAliveMinorsIDs()) do
 		
 		local player = Players[i]
 		if GLOBAL_FREE_CITY_STATES[i] and not player:IsAlive() then
 			GLOBAL_FREE_CITY_STATES[i] = nil
 		end
 
-		if player:IsAlive() and not(player:IsFreeCities() or player:IsMajor() or player:IsBarbarian()) then
+		if player:IsAlive() then
 			if Players[player:GetInfluence():GetSuzerain()]and GLOBAL_FREE_CITY_STATES[i] then
 				print("City-State has Suzerain. Removing from list")
 				GLOBAL_FREE_CITY_STATES[i] = nil
@@ -41,31 +49,41 @@ function UpdateSuzerainStatus()
 end
 
 function OnEaglelandStartTurn(id)
+	print("eagle")
 	if not Players[id]:IsAlive() then return end
+	print("has landed")
 
-	local player = Players[id]
+	local eagleland = Players[id]
 	if IsEagleland(id) then
 		UpdateSuzerainStatus()
-
-		local cities = player:GetCities()
-		if #cities > #GLOBAL_EAGLELAND_SUZERAINS[id] then
+		
+		print("is eagle")
+	
+		local numCities = eagleland:GetCities():GetCount()
+		print(numCities, #GLOBAL_EAGLELAND_SUZERAINS[id])
+		if numCities > GetTableLength(GLOBAL_EAGLELAND_SUZERAINS[id]) then
 			print("Checking if Eagleland can receive a City-State")
 			local cityStates = GLOBAL_FREE_CITY_STATES
 			--Rules: Eagleland must have met the city-state
 			--Current game turn - turn city-state added to GLOBAL_FREE_CITY_STATES >= 5
-			local numSuzerainsToGive = #cities - #GLOBAL_EAGLELAND_SUZERAINS[id]
+			local numSuzerainsToGive = numCities - GetTableLength(GLOBAL_EAGLELAND_SUZERAINS[id])
 			for k, v in pairs(GLOBAL_FREE_CITY_STATES) do
 				local cityState = Players[k]
-				if Game.GetCurrentGameTurn() - v >= 5 and player:GetDiplomacy():HasMet(k) then
-					numSuzerainsToGive = numSuzerainsToGive - 1
+				if not cityState:IsAlive() then
+					GLOBAL_FREE_CITY_STATES[k] = nil
+				else
+					if Game.GetCurrentGameTurn() - v >= 5 and eagleland:GetDiplomacy():HasMet(k) then
+						numSuzerainsToGive = numSuzerainsToGive - 1
 
-					local tokens = 0
-					while not Players[cityState:GetInfluence():GetSuzerain()] do
-						player:GetInfluence():GiveFreeTokenToPlayer(k)
-						tokens = tokens + 1
+						local tokens = 0
+						while not Players[cityState:GetInfluence():GetSuzerain()] do
+							eagleland:GetInfluence():GiveFreeTokenToPlayer(k)
+							tokens = tokens + 1
+						end
+						GLOBAL_EAGLELAND_SUZERAINS[id][k] = tokens
 					end
-					GLOBAL_EAGLELAND_SUZERAINS[id][k] = tokens
 				end
+
 				if numSuzerainsToGive <= 0 then break end
 			end
 
@@ -79,20 +97,21 @@ function InitNewGame()
 	GLOBAL_EAGLELAND_SUZERAINS = TSL.ReadMyCustomData("GRAM_EAGLELAND_SUZERAINS") or {}
 	GLOBAL_FREE_CITY_STATES = TSL.ReadMyCustomData("GRAM_FREE_CITY_STATES") or {}
 	if TSL.ReadMyCustomData("GRAM_EAGLELAND_INIT") then return end
-	for i = 0, GameDefines.MAX_PLAYERS-1, 1 do
+	for i = 0, GameDefines.MAX_PLAYERS-3, 1 do
 		local player = Players[i]
 		if player:WasEverAlive() and player:IsAlive() then
+			print(IsEagleland(i))
 			if IsEagleland(i) then
 				GLOBAL_EAGLELAND_SUZERAINS[i] = {}
 			end
 		
-			if not(player:IsFreeCities() or player:IsMajor() or player:IsBarbarian()) then
+			if not player:IsMajor() then
 				GLOBAL_FREE_CITY_STATES[i] = Game.GetCurrentGameTurn()
 			end
 		end
-	end
-
-	if #GLOBAL_EAGLELAND_SUZERAINS > 0 then
+	end 
+	print(#GLOBAL_EAGLELAND_SUZERAINS)
+	if GetTableLength(GLOBAL_EAGLELAND_SUZERAINS) > 0 then
 		--TODO Hook our functions
 		GameEvents.PlayerTurnStarted.Add(OnEaglelandStartTurn)
 	end
