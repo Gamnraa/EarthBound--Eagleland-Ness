3ee9874c992ba65e69bfb3e70c0d6bdcd19b1f12
Gamnraa <ltaz1466@gmail.com>
2023-04-15 20:23:35 -0400

    Diplomatic Favor per turn from natural wonders functional

--- Core/Trait.sql
+++ Core/Trait.sql
@@ -7,18 +7,13 @@
 --Most of these functionalities will be coded in Lua
 
 INSERT INTO Types
-			(Type,														Kind		)
-VALUES		('TRAIT_CIVILIZATION_GRAM_EAGLELAND_SOVEREIGNTY',			'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND',						'KIND_TRAIT'),
+			(Type,														Kind			)
+VALUES		('TRAIT_CIVILIZATION_GRAM_EAGLELAND_SOVEREIGNTY',			'KIND_TRAIT'	),
+			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND',						'KIND_TRAIT'	),
 
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_1',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_2',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_3',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_4',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_5',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_6',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_7',						'KIND_TRAIT'),
-			('TRAIT_LEADER_GRAM_NESS_EARTHBOUND_8',						'KIND_TRAIT');
+			('MODIFIER_GRAM_FAVOR_NATURAL_WONDERS',						'KIND_MODIFIER'	),
+			('MODIFIER_GRAM_NESS_GIVE_DIPLO_FAVOR',						'KIND_MODIFIER'	),
+			('MODIFIER_GRAM_NESS_GIVE_CULTURE',							'KIND_MODIFIER'	);
 
 INSERT INTO Traits
 			(TraitType,													Name,														Description												)
@@ -46,42 +41,75 @@ VALUES		('TRAIT_LEADER_GRAM_NESS_EARTHBOUND',		'GRAM_NESS_DIPLO_FAVOR_NATURAL_WO
 
 
 INSERT INTO Modifiers
-			(ModifierId,										ModifierType,									OwnerRequirementSetId							)
-VALUES		('GRAM_NESS_DIPLO_FAVOR_NATURAL_WONDERS',			'MODIFIER_GRAM_FAVOR_NATURAL_WONDERS'			'REQSET_GRAM_CITY_OWNS_NATURAL_WONDER'			);
+			(ModifierId,										ModifierType,									
+			OwnerRequirementSetId,								SubjectRequirementSetId						)
+VALUES		('GRAM_NESS_DIPLO_FAVOR_NATURAL_WONDERS',			'MODIFIER_GRAM_FAVOR_NATURAL_WONDERS',			
+			'REQSET_GRAM_MAP_HAS_NATURAL_WONDER',				'REQSET_GRAM_CITY_OWNS_NATURAL_WONDER'		),
+			('GRAM_NESS_GIVE_DIPLO_FAVOR',						'MODIFIER_GRAM_NESS_GIVE_DIPLO_FAVOR'		),
+			NULL,												NULL										),
+			('GRAM_NESS_GIVE_CULTURE',							'MODIFIER_GRAM_NESS_GIVE_CULTURE'			),
+			NULL,												NULL										);
 
 
 
 INSERT INTO DynamicModifiers
-			(ModifierType,								CollectionType,				EffectType										)
-VALUES		('MODIFIER_GRAM_FAVOR_NATURAL_WONDERS',		'COLLECTION_OWNER_CITY',	'EFFECT_ADJUST_PLAYER_EXTRA_FAVOR_PER_TURN'		);
+			(ModifierType,								CollectionType,					EffectType										)
+VALUES		('MODIFIER_GRAM_FAVOR_NATURAL_WONDERS',		'COLLECTION_PLAYER_CITIES',		'EFFECT_ADJUST_PLAYER_EXTRA_FAVOR_PER_TURN'		),
+			('MODIFIER_GRAM_GIVE_DIPLO_FAVOR',			'COLLECTION_OWNER',				'EFFECT_ADD_PLAYER_FAVOR'						),
+			('MODIFIER_GRAM_GIVE_CULTURE',				'COLLECTION_OWNER',				'EFFECT_ADJUST_PLAYER_YIELD_CHANGE'				);
 
 
 
 INSERT INTO ModifierArguments
-			(ModifierId									Name,			Value		)
-VALUES		('GRAM_NESS_DIPLO_FAVOR_NATURAL_WONDERS',	'Amount',		'1'			);
+			(ModifierId,								Name,			Value		)
+VALUES		('GRAM_NESS_DIPLO_FAVOR_NATURAL_WONDERS',	'Amount',		'2'			),
+			('GRAM_NESS_GIVE_DIPLO_FAVOR',				'Amount',		'50'		),
+			('GRAM_NESS_GIVE_CULTURE',					'Amount',		'50'		);
 		
 
 
 INSERT INTO RequirementSets
-			(RequirementSetId,							RequirementSetType					)
-VALUES		('REQSET_GRAM_CITY_OWNS_NATURAL_WONDER',		'REQUIREMENTSET_TEST_ANY'			);
+			(RequirementSetId,								RequirementSetType					)
+VALUES		('REQSET_GRAM_CITY_OWNS_NATURAL_WONDER',		'REQUIREMENTSET_TEST_ANY'			),
+			('REQSET_GRAM_MAP_HAS_NATURAL_WONDER',			'REQUIREMENTSET_TEST_ANY'			);
 
 
 
 INSERT INTO Requirements
-			(RequirementId,									RequirementType)
-VALUES		'REQUIREMENT_GRAM_CITY_OWNS_NATURAL_WONDER',	'REQUIREMENT_CITY_HAS_FEATURE'	);
+			(RequirementId,									RequirementType						)
+SELECT		'REQUIREMENT_GRAM_CITY_OWNS_' || FeatureType,	'REQUIREMENT_CITY_HAS_FEATURE'	
+FROM Features WHERE NaturalWonder = '1';
+
+
+INSERT INTO Requirements
+			(RequirementId,									RequirementType						)
+SELECT		'REQUIREMENT_GRAM_MAP_HAS_' || FeatureType,		'REQUIREMENT_MAP_HAS_FEATURE'	
+FROM Features WHERE NaturalWonder = '1';
 
 
 
 INSERT INTO RequirementArguments
-			(RequirementId,									Name,				Value			)
-SELECT		'REQUIREMENT_GRAM_CITY_OWNS_NATURAL_WONDER',	'FeatureType',		FeatureType	
-FROM Features WHERE NaturalWonder = '1';	
+			(RequirementId,			Name,				Value					)
+SELECT		R1.RequirementId,		'FeatureType',		F1.FeatureType	
+FROM Requirements R1, Features F1 WHERE R1.RequirementId LIKE 'REQUIREMENT_GRAM_CITY_OWNS_%' AND substr(R1.RequirementId, 28) = F1.FeatureType AND  F1.NaturalWonder = '1';	
+
+
+
+INSERT INTO RequirementArguments
+			(RequirementId,			Name,				Value					)
+SELECT		R1.RequirementId,		'FeatureType',		F1.FeatureType	
+FROM Requirements R1, Features F1 WHERE R1.RequirementId LIKE 'REQUIREMENT_GRAM_MAP_HAS_%' AND substr(R1.RequirementId, 26) = F1.FeatureType AND  F1.NaturalWonder = '1';	
 
 
 
 INSERT INTO RequirementSetRequirements
-			(RequirementSetId,								RequirementId								)
-VALUES		('REQSET_GRAM_CITY_OWNS_NATURAL_WONDER',		'REQUIREMENT_GRAM__CITY_OWNS_NATURAL_WONDER');
+			(RequirementSetId,							RequirementId	)
+SELECT		'REQSET_GRAM_CITY_OWNS_NATURAL_WONDER',		RequirementId
+FROM Requirements WHERE RequirementId LIKE 'REQUIREMENT_GRAM_CITY_OWNS_%';
+
+
+
+INSERT INTO RequirementSetRequirements
+			(RequirementSetId,							RequirementId	)
+SELECT		'REQSET_GRAM_MAP_HAS_NATURAL_WONDER',		RequirementId
+FROM Requirements WHERE RequirementId LIKE 'REQUIREMENT_GRAM_MAP_HAS_%';
